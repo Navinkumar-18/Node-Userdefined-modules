de62b4b9662e35a108332ba7fddd520361250ef5
Navinkumar-18 <197935937+Navinkumar-18@users.noreply.github.com>
2026-04-10 13:45:55 +0530

    second change

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const names=require('./one');
 const sayHi=require('./two');
+(require('./mind_grenade'));
 
 sayHi(names.john);
 sayHi(names.peter);
+console.log(names);
---
+++ mind_grenade.js
@@ -0,0 +1,7 @@
+const a=10;
+const b =5;
+
+function addValue(a,b){
+    console.log('The sum of',a,'and ',b,'is',a+b);
+}
+addValue(a,b);
